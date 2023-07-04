3452212e33bd7848d8d1e91ebabf8fb45263dd78
Praveen S <praveensudharsanan@gmail.com>
2023-07-04 11:16:56 -0400

    Added the query to fetch the data from neo4j

---
+++ neo4j_calls/neo4j_api.js
@@ -0,0 +1,84 @@
+let neo4j = require('neo4j-driver');
+let { creds } = require("./../config/credentials");
+let driver = neo4j.driver("bolt://0.0.0.0:7687", neo4j.auth.basic(creds.neo4jusername, creds.neo4jpw));
+
+
+//Get all number of nodes//
+exports.get_num_nodes = async function () {
+    let session = driver.session();
+    const num_nodes = await session.run('MATCH (n) RETURN n', {
+    });
+    session.close();
+    console.log("RESULT", (!num_nodes ? 0 : num_nodes.records.length));
+    return (!num_nodes ? 0 : num_nodes.records.length);
+};
+
+//Create a user//
+exports.create_user = async function (name) {
+    let session = driver.session();
+    let user = "No User Was Created";
+    try {
+        user = await session.run('MERGE (n:user {name: $id}) RETURN n', {
+            id: name
+        });
+    }
+    catch (err) {
+        console.error(err);
+        return user;
+    }
+    return user.records[0].get(0).properties.name;
+}
+
+//Delete a node//
+exports.deleteNode = async function (nodeId) {
+    let session = driver.session();
+    const result = await session.run('MATCH (n) WHERE ID(n) = $nodeId DELETE n', { nodeId: neo4j.int(nodeId) });
+    session.close();
+  
+    console.log(`Deleted ${result.summary.counters.nodesDeleted()} node(s).`);
+    return result.summary.counters.nodesDeleted();
+  };
+
+//Get all nodes by name//
+  exports.getNodeAndRelationsByName = async function (nodeName) {
+  let session = driver.session();
+
+  const result = await session.run(
+    'MATCH (n {name: $nodeName})-[r]-(relatedNode) RETURN n, r, relatedNode',
+    { nodeName }
+  );
+
+  session.close();
+
+  const nodes = [];
+  const relationships = [];
+
+  result.records.forEach((record) => {
+    const node = record.get('n').properties;
+    const relation = record.get('r').properties;
+    const relatedNode = record.get('relatedNode').properties;
+
+    nodes.push(node);
+    relationships.push({ relation, relatedNode });
+  });
+
+  return { nodes, relationships };
+};
+
+//Get all nodes by label//
+exports.getNodesByLabel = async function (label) {
+  let session = driver.session();
+
+  const result = await session.run(
+    'MATCH (n:' + label + ') RETURN n'
+  );
+
+  session.close();
+
+  const nodes = result.records.map((record) => record.get('n').properties);
+  return nodes;
+};
+
+
+  
+
---
+++ routes/test_api.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const neo4j_calls = require('./../neo4j_calls/neo4j_api');
+
+router.get('/', async function (req, res, next) {
+    res.status(200).send("Root Response from :8080/test_api")
+    return 700000;
+})
+
+//Api end point Get all nodes//
+router.get('/neo4j_get', async function (req, res, next) {
+    let result = await neo4j_calls.get_num_nodes();
+    console.log("RESULT IS", result)
+    res.status(200).send({ result })    //Can't send just a Number; encapsulate with {} or convert to String.     
+    return { result };
+})
+
+//Api end point to create a user 
+router.post('/neo4j_post', async function (req, res, next) {
+    //Passing in "name" parameter in body of POST request
+    let { name } = req.body;
+    let string = await neo4j_calls.create_user(name);
+    res.status(200).send("User named " + string + " created")
+    return 700000;
+    //res.status(200).send("test delete")
+})
+
+
+//Api end point to delete a node
+router.delete('/neo4j_delete/:nodeId', async function (req, res, next) {
+  const nodeId = req.params.nodeId; // Get the node ID from the request parameters
+
+  let nodesDeleted;
+  try {
+    nodesDeleted = await neo4j_calls.deleteNode(nodeId);
+    console.log(`Deleted ${nodesDeleted} node(s).`);
+  } catch (error) {
+    console.error('Error deleting node:', error);
+    res.status(500).send({ error: 'Failed to delete node.' });
+    return;
+  }
+
+  res.status(200).send({ nodesDeleted });
+});
+
+//Api end point to get all nodes by name
+router.get('/neo4j_getnodename/:nodeName', async function (req, res, next) {
+  const nodeName = req.params.nodeName;
+
+  try {
+    const result = await neo4j_calls.getNodeAndRelationsByName(nodeName);
+    console.log("RESULT IS", result);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error retrieving node and relationships:', error);
+    res.status(500).json({ error: 'Failed to retrieve node and relationships.' });
+  }
+});
+
+//Api end point to get all nodes by label
+router.get('/neo4j_getlabel/:label', async function (req, res, next) {
+  const label = req.params.label;
+
+  try {
+    const result = await neo4j_calls.getNodesByLabel(label);
+    console.log("RESULT IS", result);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error retrieving nodes:', error);
+    res.status(500).json({ error: 'Failed to retrieve nodes.' });
+  }
+});
+
+
+
+
+
+module.exports = router;
